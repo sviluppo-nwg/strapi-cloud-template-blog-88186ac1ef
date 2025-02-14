f3bef50417829880c55650b5b9abae71df0bab7a
Cristian Accetta <cristian@oblolab.it>
2025-02-14 16:41:06 +0100

    update

--- strapi/config/functions/bootstrap.ts
+++ strapi/config/functions/bootstrap.ts
@@ -1,4 +1,23 @@
 export default async ({ strapi }) => {
-  const knex = strapi.db.connection; // Usa la connessione DB di Strapi
-  await knex.migrate.latest(); // Esegui tutte le migrations
+  const superAdminRole = await strapi.query('admin::role').findOne({ where: { code: 'strapi-super-admin' } });
+  
+  const adminExists = await strapi.query('admin::user').findOne({ where: { email: 'cristian@oblolab.it' } });
+
+  if (!adminExists) {
+    await strapi.query('admin::user').create({
+      data: {
+        firstname: 'Cristian',
+        lastname: 'Accetta',
+        email: 'cristian@oblolab.it',
+        password: 'SecurePassword123',
+        roles: [superAdminRole.id],
+        isActive: true,
+      },
+    });
+  } else if (!adminExists.roles || adminExists.roles.length === 0) {
+    await strapi.query('admin::user').update({
+      where: { email: 'cristian@oblolab.it' },
+      data: { roles: [superAdminRole.id] },
+    });
+  }
 };
