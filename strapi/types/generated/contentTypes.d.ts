18e2ca7d9896b877b419c7af99a8bbcc0472a7bb
Cristian Accetta <cristian@oblolab.it>
2025-04-17 12:16:48 +0200

    add appVisibility in "avvisi"

--- strapi/types/generated/contentTypes.d.ts
+++ strapi/types/generated/contentTypes.d.ts
@@ -441,6 +441,7 @@ export interface ApiAvvisiAvvisi extends Struct.CollectionTypeSchema {
     draftAndPublish: true;
   };
   attributes: {
+    appVisibility: Schema.Attribute.Boolean;
     attivo: Schema.Attribute.Boolean;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
